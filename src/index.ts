49e1e60abe338a2f19af879eb56919bf89900f33
Ivan Pantic <panta@talentkit.io>
2021-10-14 22:18:23 +0200

    Change DEFAULT_OPTIONS export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { AdequateCache } from './adequate_cache';
-import {
-  DEFAULT_OPTIONS as ADEQUATE_CACHE_DEFAULT_OPTIONS,
-  IAdequateCacheOptions,
-} from './options';
+import { DEFAULT_OPTIONS, IAdequateCacheOptions } from './options';
 
-export { AdequateCache, IAdequateCacheOptions, ADEQUATE_CACHE_DEFAULT_OPTIONS };
+export { AdequateCache, IAdequateCacheOptions, DEFAULT_OPTIONS };
 export default AdequateCache;
